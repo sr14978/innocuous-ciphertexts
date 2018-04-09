4b1193de5260553e939d3f3330a9148314577208
sr14978 <sr14978@my.bristol.ac.uk>
2018-04-09 09:24:27 -0700

    bins

--- bins.py
+++ bins.py
@@ -3,26 +3,44 @@
 import argparse
 import pickle
 
-def sort(filename_in):
+modes = {
+	'CHARACTER_DISTROBUTION':'char',
+	'SLASHES_FREQUENCY':'slash'
+}
 
-	bins = [0] * 256
+def sort(filename_in, mode=modes['CHARACTER_DISTROBUTION']):
 
-	with open(filename_in, "rb") as f:
-		lines = pickle.load(f)
+	if mode == modes['CHARACTER_DISTROBUTION']:
 
-	for line in lines:
-		for chr in line:
-			bins[ord(chr)] += 1
+		bins = [0] * 256
+		with open(filename_in, "rb") as f:
+			lines = pickle.load(f)
 
-	return bins	
+		for line in lines:
+			for chr in line:
+				bins[ord(chr)] += 1
+		return bins
+
+	elif mode == modes['SLASHES_FREQUENCY']:
+
+		bins = [0] * 2
+		with open(filename_in, "rb") as f:
+			lines = pickle.load(f)
+
+		for line in lines:
+			for chr in line:
+				bins[1 if chr == '/' else 0] += 1
+		return bins
 
 if __name__ == "__main__":
 
 	parser = argparse.ArgumentParser()
 	parser.add_argument('-i', '--in', default="reference_urls")
 	parser.add_argument('-o', '--out', default="reference_bins")
+	parser.add_argument('-m', '--mode',
+		choices=modes.values(), default=modes['CHARACTER_DISTROBUTION'])
 	args = vars(parser.parse_args())
 
 	with open(args["out"], "wb") as f:
-		pickle.dump(sort(args["in"]), f)
-	
+		pickle.dump(sort(args["in"], args["mode"]), f)
+
