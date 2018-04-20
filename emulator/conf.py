d59c1746fdf2764198ce3cc79fd945e1c3f7d504
sr14978 <sr14978@my.bristol.ac.uk>
2018-04-20 18:29:59 +0100

    fix

--- emulator/conf.py
+++ emulator/conf.py
@@ -6,7 +6,7 @@ size_of_char = 0x100
 divisions = 10
 URI_length = 256
 
-wrapped_length = URI_size + 18
+wrapped_length = URI_length + 18
 
 frag_ciphertext_length = int(URI_length*math.log(divisions)/math.log(size_of_char)) -1
 frag_plaintext_padded_length = frag_ciphertext_length - Encrypter._CTXT_EXPANSION
