98ecd8aa58a6e8e91b98a906a833e5d8dc3bcf62
sr14978 <sr14978@my.bristol.ac.uk>
2018-04-28 19:59:18 +0100

    optimisations to improve speed

--- emulator/emulate_pattern.py
+++ emulator/emulate_pattern.py
@@ -4,23 +4,24 @@
 import numpy as np
 from collections import deque
 import random
+import itertools
 
 import common as com
 import common_dict as comd
 
-def init_emulator(bins, base=100, message_length=256, pattern_length=40):
+def init_emulator(bins, base=100, message_length=256, pattern_length=32):
 	"""Returns (encode, decode) functions that emulate the character distrobution defined by `bins`."""
 	message_max = (message_length+1)*8
 	cumlative_splits = comd._create_cumlative_splits(bins)
 	comd._set_dist(cumlative_splits)
 	digit_splits = comd._create_digit_splits(bins, base, cumlative_splits)
 	base_powers = com._calculate_powers_of_base(base, message_max)
+	pattern_length_rng = range(pattern_length)
 	def encode(message):
 		message = '\xFF'+message
 		input_value = 0
 		for char in message:
-			input_value <<= 8
-			input_value += ord(char)
+			input_value = (input_value << 8) | ord(char)
 		if input_value > (1<<message_max):
 			raise Exception("Message too long")
 		digits = deque()
@@ -34,7 +35,8 @@ def init_emulator(bins, base=100, message_length=256, pattern_length=40):
 					non_zero_digit_not_found = False
 			digits.appendleft(digit)
 			input_value -= digit*mod
-		bit_stream = []
+		bit_stream = [0] * (pattern_length * len(digits))
+		i = 0
 		for digit in digits:
 			low_split, high_split = digit_splits[digit]
 			value = low_split + random.random()*(high_split-low_split)
@@ -42,42 +44,36 @@ def init_emulator(bins, base=100, message_length=256, pattern_length=40):
 			value = index
 			# value = index + int(0.2e7)	# bin offset
 			# value = (index + 30)	# bin offset
-			bit_stream += to_pattern_bits(value)
-		character_stream = []
-		while len(bit_stream) > 0:
-			character_stream.append(chr(evaluate(bit_stream[:8])))
-			bit_stream = bit_stream[8:]
-		return "".join(character_stream)
+			for j in pattern_length_rng:
+				bit_stream[i] = ((value>>j)&1)
+				i += 1
+		return "".join(chr(evaluate8(chunk)) for chunk in group(bit_stream, 8))
 
-	def to_pattern_bits(value):
-        	return [(value>>i)&1 for i in range(pattern_length)]
+	def group(xs, n): return itertools.izip(*(itertools.islice(xs, i, None, n) for i in xrange(n)))
+
+	def evaluate8(bits):
+		a, b, c, d, e, f, g, h = bits
+		return (h << 7 | g << 6 | f << 5 | e << 4 | d << 3 | c << 2 | b << 1 | a)
 
 	def evaluate(bits):
-        	sum = 0
-	        for bit in reversed(bits):
-        	        sum <<= 1
-                	sum += bit
-	        return sum
+			sum = 0
+			for bit in reversed(bits): sum = (sum << 1) | bit
+			return sum
 
 	def to_char_bits(value):
-                return [(value>>i)&1 for i in range(8)]
+		return [(value>>i)&1 for i in xrange(8)]
 
 
 	def decode(url):
 
 		bit_stream = [b for c in url for b in to_char_bits(ord(c))]
-		pattern_stream = []
-		while len(bit_stream) > 0:
-			pattern_stream.append(evaluate(bit_stream[:pattern_length]))
-			bit_stream = bit_stream[pattern_length:]
-		digits = deque()
-		for pattern in pattern_stream:
-			index = pattern
-			# index = pattern - int(0.2e7)
-			# index = pattern - 30
-			value,_ = cumlative_splits[index]
-			digit = com._lies_at_index_range(digit_splits, value)
-			digits.append(digit)
+		digits = [evaluate(chunk) for chunk in group(bit_stream, pattern_length)]
+		l = len(digits)
+		i = 0
+		while i < l:
+			# digits[i] = com._lies_at_index_range(digit_splits, cumlative_splits[digits[i] - 30][0])
+			digits[i] = com._lies_at_index_range(digit_splits, cumlative_splits[digits[i]][0])
+			i+=1
 		output_value = 0
 		for digit,mod in zip(digits, base_powers[::-1]):
 			output_value += digit*mod
