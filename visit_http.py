3454a5a05ff8c25e7d25072c39f82b704744ac60
sr14978 <sr14978@my.bristol.ac.uk>
2018-04-28 13:19:25 +0100

    updated crawler

--- visit_http.py
+++ visit_http.py
@@ -9,11 +9,12 @@ This simple web crawler program will follow links on webpages to create normal l
 
 from collections import deque
 from HTMLParser import HTMLParser
-import urllib2, urlparse
+import urlparse
+import traceback, sys
+import urllib2
 
 class MyHTMLParser(HTMLParser):
 	def handle_starttag(self, tag, attrs):
-		attrs = dict(attrs)
 		if tag == 'a':
 			attr = "href"
 		elif tag == "img":
@@ -24,6 +25,10 @@ class MyHTMLParser(HTMLParser):
 			attr = "href"
 		else:
 			return
+		if self.count > 5:
+                        return
+                self.count += 1
+		attrs = dict(attrs)
 		if attr not in attrs:
 			return
 		link = attrs[attr]
@@ -34,7 +39,7 @@ class MyHTMLParser(HTMLParser):
 			return
 		elif link.startswith("#"):
 			return
-		elif "javascript:void(0)" in link:
+		elif "javascript" in link:
 			return
 		elif link.startswith("http"):
 			pass
@@ -46,36 +51,45 @@ class MyHTMLParser(HTMLParser):
 			link = base_url + link
 		else:
 			obj = urlparse.urlparse(self.url)
-			path = obj.path[:-1] if obj.path[-1] == '/' else obj.path
+			path = obj.path if len(obj.path) == 0 or obj.path[-1] != '/' else obj.path[:-1]
 			base_url = "http://" + obj.hostname + path
 			link = base_url + '/' + link
 		if link not in visited:
 			queue.appendleft(link)
 			visited.append(link)
-
+			print "ADDED", link
 
 if __name__ == "__main__":
-	seed_sites = ["exeter", "dur", "manchester", "cam", "ox", "bath", "imperial", "ucl", "bristol", "uwe"]
 
+	seed_sites = ["exeter", "manchester", "cam", "ox", "bath",
+			"imperial", "ucl", "bristol", "uwe"]
 	visited = ["http://www.%s.ac.uk/"%(uni) for uni in seed_sites]
+
+	# with open("good_urls", "r") as f:
+	#	seed_sites = [l[:-1] for l in f.readlines()]
+	# visited = ["http://" + site for site in seed_sites]
+
 	queue = deque(visited)
 	parser = MyHTMLParser()
 
 	while len(queue) > 0:
 		url = queue.pop()
-		print url, len(queue)
+		print "queue", len(queue)
+		print "visiting", url
 		try:
 			request = urllib2.urlopen(url)
 			if request.getcode() != 200:
 				continue
 			if request.geturl().startswith("https"):
+				print "redirected to https"
 				continue
 			if len(queue) > 10000:
 				continue
 			if 'text/html' in request.headers.getheader('content-type'):
 				contents = request.read()
+				parser.count = 0
 				parser.url = url
 				parser.feed(contents)
 
 		except:
-			continue
+			traceback.print_exc(file=sys.stdout)
